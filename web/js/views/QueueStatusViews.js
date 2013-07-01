16209497be7984f73fa12dfa7ecbf4165b965c9b
Mark Wilkie <mark@buzzfeed.com>
2013-06-30 21:02:14 -0400

    log js and css updates

--- web/js/views/QueueStatusViews.js
+++ web/js/views/QueueStatusViews.js
@@ -53,12 +53,24 @@ var QueueOKView = Backbone.View.extend( {
 });
 
 /**
+ * View for crawl status Header
+ */
+var CrawlStatusHeader = Backbone.View.extend( {
+    tagName: 'tr',
+    render: function() {
+        //console.log(workers);
+        template = _.template($('#crawl-status-header').html(), {});
+        this.$el.html(template);
+        return this;
+    }
+});
+
+/**
  * View for crawl status
  */
 var CrawlStatus = Backbone.View.extend( {
-    tagName: 'div',
+    tagName: 'tr',
     render: function(crawl_status) {
-        //console.log(workers);
         template = _.template($('#crawl-status').html(), crawl_status);
         this.$el.html(template);
         return this;
@@ -73,6 +85,7 @@ var CrawlStatusesView = Backbone.View.extend( {
     render: function(craw_statuses) {
         var self = this;
         self.$el.html('');
+        self.$el.append((new CrawlStatusHeader()).render().$el);
         _.each(craw_statuses, function(crawl_status, i) {
              self.$el.append((new CrawlStatus()).render(crawl_status).$el);
          });
@@ -80,10 +93,35 @@ var CrawlStatusesView = Backbone.View.extend( {
          // event for log click
          $('.log-link').click( function(ev) {
              $(this).attr('href');
-             console.log($(this).attr('href'));
+             crawl_log_view = new CrawlLogView();
+             crawl_log_view.render();             
+             $('#log').css('top',$(document).scrollTop() + 'px');
              return false;
          });
 
+         // close log window with escape
+         $(document).keyup(function(e) {
+             if (e.keyCode == 27) {
+                 $('#log').hide();
+             }
+         });
+
+        return this;
+    }
+});
+
+
+/**
+ * View for crawl log
+ */
+var CrawlLogView = Backbone.View.extend( {
+    el: $('#log'),
+    render: function(log) {
+        var self = this;
+        self.$el.html('');
+        template = _.template($('#log-template').html(), {});
+        this.$el.html(template);
+        $('#log').show();
         return this;
     }
 });
